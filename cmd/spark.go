c91999b316bb730d62ef90731de57e70e0e6e5fa
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-02-06 11:10:33 +0100

    Add raw2science job

--- cmd/spark.go
+++ cmd/spark.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -26,16 +27,18 @@ to quickly create a Cobra application.`,
 	},
 }
 
+// TODO split parameters for stream2raw and raw2science
 var sparkArgs = map[string]interface{}{
 	"image":                 "delicious",
 	"producer":              "delicious",
 	"kafka_socket":          "delicious",
 	"kafka_topic":           "delicious",
 	"fink_alert_schema":     "delicious",
-	"kafka_starting_sffset": "delicious",
+	"kafka_starting_offset": "delicious",
 	"online_data_prefix":    "delicious",
 	"fink_trigger_update":   "delicious",
 	"log_level":             "delicious",
+	"night":                 "delicious",
 }
 
 func init() {
@@ -57,4 +60,49 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// sparkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func runSpark(sparkArgs map[string]interface{}) {
+
+	_, config := setKubeClient()
+
+	apiServerUrl := config.Host
+
+	sparkArgs["api_server_url"] = apiServerUrl
+
+	cmdTpl := `spark-submit --master "k8s://{{ .api_server_url }}" \
+    --deploy-mode cluster \
+    --conf spark.executor.instances=1 \
+    --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+    --conf spark.kubernetes.container.image="{{ .image }}" \
+    --conf spark.driver.extraJavaOptions="-Divy.cache.dir=/home/fink -Divy.home=/home/fink" \
+    $ci_opt \
+    local:///home/fink/fink-broker/bin/{{ .bin }} \
+    -log_level "{{ .log_level }}" \
+    -online_data_prefix "{{ .online_data_prefix }}" \
+    -producer "{{ .producer }}" \
+    -tinterval "{{ .fink_trigger_update }}" \
+    `
+
+	switch sparkArgs["bin"] {
+	case "stream2raw.py":
+		cmdTpl += `-servers "{{ .kafka_socket }}"
+    -schema "{{ .fink_alert_schema }}"
+    -startingoffsets_stream "{{ .kafka_starting_offset }}" \
+    -topic "{{ .kafka_topic }}"`
+
+	case "raw2science.py":
+		cmdTpl += `-night "{{ .night }}"`
+	}
+
+	cmd := format(cmdTpl, sparkArgs)
+
+	out, errout := ExecCmd(cmd)
+
+	outmsg := OutMsg{
+		cmd:    cmd,
+		out:    out,
+		errout: errout}
+
+	log.Printf("message: %v\n", outmsg)
 }
