ccdc477fa452aea09cbda8dce43c7daff6089236
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-08-23 16:29:01 +0200

    Update current feature

--- cmd/kafka.go
+++ cmd/kafka.go
@@ -13,15 +13,9 @@ import (
 // kafkaCmd represents the kafka command
 var kafkaCmd = &cobra.Command{
 	Use:   "kafka",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List kafka topics produced by the fink-broker",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("kafka called")
+		logger.Info("List kafka topics produced by the fink-broker")
 		for _, t := range getKafkaTopics() {
 			if strings.HasPrefix(t, finkPrefix) {
 				fmt.Println(t)
