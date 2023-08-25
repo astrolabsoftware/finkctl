f8c562b0e026fc45fe9a559a0c96c88e0add08fa
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-08-25 12:11:37 +0200

    Update current feature

--- cmd/wait_task.go
+++ cmd/wait_task.go
@@ -19,15 +19,17 @@ var waitTaskCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		labelValues := []string{"driver", "executor"}
 		expected_pods := 3
-		logger.Info("Wait for fink-broker pods to be launched %s", labelValues)
 		clientSet, _ := setKubeClient()
 		for _, value := range labelValues {
-			err := waitForPodExistsBySelector(clientSet, getCurrentNamespace(), "spark-role="+value, timeout, expected_pods)
+			selector := "spark-role=" + value
+			logger.Info("Wait for fink-broker pods with label '%s' to be created", selector)
+			err := waitForPodExistsBySelector(clientSet, getCurrentNamespace(), selector, timeout, expected_pods)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "error: timed out waiting for %s pods to be created, reason: %s", value, err)
 				os.Exit(1)
 			}
-			err = waitForPodReadyBySelector(clientSet, getCurrentNamespace(), "spark-role="+value, timeout)
+			logger.Info("Wait for fink-broker pods with label '%s' to be ready", selector)
+			err = waitForPodReadyBySelector(clientSet, getCurrentNamespace(), selector, timeout)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "error: timed out waiting for %s pods to be ready, reason: %s", value, err)
 				os.Exit(1)
